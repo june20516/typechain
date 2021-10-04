becec5109c2259c8ffffebd0206542ce9fd26039
Bran <june20516@gmail.com>
2021-10-04 17:46:59 +0900

    block chain을 사용하기 위한 function 작성

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number, 
+        previousHash: string, 
+        timestamp: number, 
+        data: string
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index: number,
         hash: string,
@@ -19,11 +29,16 @@ class Block {
     }
 }
 
-const genesisBlock: Block = new Block(0, '4563459', '', 'Hello', 123456)
 
-let blockchain: [Block] = [genesisBlock];
+const genesisBlock: Block = new Block(0, '4563459', '', 'Hello', 123456);
 
-blockchain.push('stuff');
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockchain);
 
